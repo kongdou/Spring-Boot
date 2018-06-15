224c0eff4019464a3e337708a0a69ccaa5ccb669
deepsky <zhao.xiao.jie@outlook.com>
2018-06-15 09:25:47 +0800

    整理代码
    整理代码

--- spring_boot_in_action/firstboot/src/main/java/fun/deepsky/springbootinaction/firstboot/ReaderRepository.java
+++
@@ -1,7 +0,0 @@
-package fun.deepsky.springbootinaction.firstboot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReaderRepository extends JpaRepository<Reader,String>{
-
-}
--- spring_boot_in_action/securedwebtest/src/main/java/fun/deepsky/springbootinaction/securedwebtest/ReaderRepository.java
+++
@@ -1,7 +0,0 @@
-package fun.deepsky.springbootinaction.securedwebtest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReaderRepository extends JpaRepository<Reader,String>{
-
-}
